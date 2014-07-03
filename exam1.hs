ebcfeae29e10d90432740da78b33990f52af6d2d
TTsurutani <tbird@mbj.nifty.com>
2014-07-03 19:35:09 +0900

    課題４月分

---
+++ exam1.hs
@@ -0,0 +1,54 @@
+module Foo where
+-- | 4/1
+-- >>> fact 0
+-- 1
+-- >>> fact 5
+-- 120
+
+fact = undefined
+
+-- | 4/2
+-- >>> take 10 fib
+-- [0,1,1,2,3,5,8,13,21,34]
+
+fib = undefined
+
+-- |  4/3
+-- >>> primes 20
+-- [2,3,5,7,11,13,17,19]
+
+primes = undefined
+
+-- | 4/7
+-- >>> lth [1,2,3]
+-- 3
+
+lth = undefined
+
+-- | 4/8 ProjectEuler
+
+euler1 = undefined
+
+-- | 4/9
+-- >>> zipWith' (+) [1,2,3] [4,5,6]
+-- [5,7,9]
+
+zipWith' = undefined
+
+-- | 4/11
+-- >>> take' 3 [1..]
+-- [1,2,3]
+
+take' = undefined
+
+-- | 4/15
+-- >>> last' [1,2,3]
+-- 3
+
+last' = undefined
+
+-- | 4/16
+-- >>> filter' (>3) [1..5]
+-- [4,5]
+
+filter' = undefined
